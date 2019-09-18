75c9412544fb1d5211c018db848a18898cf3b0b6
KrishnanSG <krishsg525@gmail.com>
2019-09-19 00:01:04 +0530

    Intial implementation of Bloom Filter

---
+++ BloomFilter.py
@@ -0,0 +1,63 @@
+import math
+import mmh3
+
+
+class BloomFilter:
+
+    # To create bit array of size n
+    '''
+      size - size of bit array calc based the formula
+      k    - no. of hash function used to hash the value
+      p    - probability of false positive
+    '''
+
+    def __init__(self, n):
+        self.p = 0.2
+        self.size = math.ceil(-n*math.log(self.p)/(math.log(2)**2))
+        self.k = math.ceil(self.size/n*math.log(2))
+        self.bit_array = [0] * self.size
+
+    # Func to insert values into BF
+    def insert(self, value):
+        for i in range(self.k):
+            index = mmh3.hash(value, i) % self.size
+            self.bit_array[index] = 1
+
+    # To check if the value is present in BF or not
+    def validate(self, value):
+        for i in range(self.k):
+            check_at_index = mmh3.hash(value, i) % self.size
+            if self.bit_array[check_at_index] == 1:
+                continue
+            else:
+                return False
+        return True
+
+    # Returns the bit array
+    def getBloomFilter(self):
+        return self.bit_array
+
+    # Returns the size of the bit arry
+    def getSize(self):
+        return self.size
+
+    # Returns the # of Hash Functions ie. h1(k), h2(k) ...
+    def getNumberOfHashFunctions(self):
+        return self.k
+
+
+no_of_lines = 10
+
+bloom_filter = BloomFilter(no_of_lines)
+bloom_filter.insert('First Line')
+bloom_filter.insert("bloom_filter.insert('Second Line')")
+bloom_filter.insert('Third Line')
+bloom_filter.insert('Fourth Line')
+bloom_filter.insert('Third Line')
+bloom_filter.insert('Fourth Line')
+
+print(bloom_filter.getBloomFilter())
+print(bloom_filter.getNumberOfHashFunctions())
+
+print(bloom_filter.validate('First Line'))
+print(bloom_filter.validate("bloom_filter.insert('Second Line')"))
