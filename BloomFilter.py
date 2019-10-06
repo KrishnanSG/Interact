103d64c64edec9f500cadb6cd71edd809644f4b2
KrishnanSG <krishsg525@gmail.com>
2019-10-06 16:48:06 +0530

    Modify BF based on freq algorithm

--- BloomFilter.py
+++ BloomFilter.py
@@ -16,22 +16,31 @@ class BloomFilter:
         self.size = math.ceil(-n*math.log(self.p)/(math.log(2)**2))
         self.k = math.ceil(self.size/n*math.log(2))
         self.bit_array = [0] * self.size
+        self.validate_array = []
 
     # Func to insert values into BF
-    def insert(self, value):
+    def insert(self, value, freq=1):
         for i in range(self.k):
-            index = mmh3.hash(value, i) % self.size
+            index = mmh3.hash(value, i+freq) % self.size
             self.bit_array[index] = 1
 
     # To check if the value is present in BF or not
-    def validate(self, value):
+    def validate(self, value, freq=1):
         for i in range(self.k):
-            check_at_index = mmh3.hash(value, i) % self.size
-            if self.bit_array[check_at_index] == 1:
+            check_at_index = mmh3.hash(value, i+freq) % self.size
+            if self.validate_array[check_at_index] == 1:
                 continue
             else:
                 return False
         return True
+
+    def readBloomFilterFromFile(self):
+        f = open("bloomfilter.bin", "rb")
+        self.validate_array = list(f.read())
+        for i in range(0, len(self.validate_array)):
+            self.validate_array[i] -= 48
+        print(self.validate_array)
+        f.close()
 
     # Returns the bit array
     def getBloomFilter(self):
@@ -44,3 +53,8 @@ class BloomFilter:
     # Returns the # of Hash Functions ie. h1(k), h2(k) ...
     def getNumberOfHashFunctions(self):
         return self.k
+
+
+# bf = BloomFilter(5)
+# bf.readBloomFilterFromFile()
+# print(bf.validate('print("something")'))
