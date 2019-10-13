d520669879d3ef53d80fbb832cdda2e735c7caf6
Sivagiri Visakan <siva.g.visakan@gmail.com>
2019-10-13 20:08:20 +0530

    P2P/Server: Remove unused code and change the max data that can be received in a request.

--- P2P/Server.py
+++ P2P/Server.py
@@ -4,16 +4,9 @@ import time
 
 from . import utils
 
+MAX_RECEIVABLE_CONTENT_SIZE = 500000
 class NetworkManager:
 
-    REQUEST_BLOOMFILTER  = 2  
-    REQUEST_BLOOMFILTER_PADDING  = [5,4,3,1]
-
-    REQUEST_ACKNOWLEDGE_SEND_BLOOMFILTER = 3 # Acknowledge two and send bloomfilter
-    REQUEST_ACKNOWLEDGE_SEND_BLOOMFILTER_PADDING  = [1,3,5,1]
-
-
-    REQUEST_SEND_ACTUAL_LINES = 4
     # Variable to tell if the current user is host or client
     status = ""
     queue = []
@@ -23,11 +16,7 @@ class NetworkManager:
 
     def check_if_incoming_data(self):
         try:
-            # TODO: Make it work for more than 1kb of data
-            data = self.socket.recv(1024)
-
-            # print("Data received in ", self.status, ' data=', repr(data))
-
+            data = self.socket.recv(MAX_RECEIVABLE_CONTENT_SIZE)
             req = utils.parse_received_data(data)
             self.handler.handle_request(req)
             return tuple([00, ''])
@@ -36,21 +25,6 @@ class NetworkManager:
             # No data to read
             return tuple([00, ''])
 
-
-    def send_data(self, data, request_type):
-        # if(self.status=="client"):
-            # print(self.socket)
-            if request_type == self.REQUEST_BLOOMFILTER:
-                padding = bytes(self.REQUEST_BLOOMFILTER_PADDING)
-            elif request_type == self.REQUEST_ACKNOWLEDGE_SEND_BLOOMFILTER:
-                padding = bytes(self.REQUEST_ACKNOWLEDGE_SEND_BLOOMFILTER_PADDING)
-
-            print("Sending data...", data)
-            if isinstance(data, bytes):
-                data = padding + data
-                self.socket.send(data)
-            else:
-                self.socket.send(padding + str.encode(data))
 
     def send_request(self, request):
             print("Sending data...")
@@ -85,5 +59,3 @@ class NetworkManager:
         client.connect((ip,port))
         client.setblocking(False)
         print("\nHi ",host," you are succesfully connected to")
-        print("IP:",ip)
-        print("Port:",port)
