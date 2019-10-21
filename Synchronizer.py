606ff70cb657b7b897b033c7933bcaaf09a1f25d
KrishnanSG <krishsg525@gmail.com>
2019-10-21 19:07:38 +0530

    Fix major bug casued by deletion. Cause: Line number wasn't updated.

--- Synchronizer.py
+++ Synchronizer.py
@@ -2,29 +2,42 @@ import mmh3
 def syncFile(filename,my_missing_content,received_missing_content):
     
     file_content=[]
-    lines_to_insert=[]
+    contents_to_modify=[]
 
     # Read file
     with open(filename,'r') as f:
         file_content=f.readlines()
 
     for i in received_missing_content:
-        
         if i in my_missing_content:
             # Present so update contents
             file_content[i-1]=received_missing_content[i]
             del(my_missing_content[i])
         else:
             # Not Present so insert no line
-            lines_to_insert.append(i)
+            # lines_to_insert.append(i)
+            contents_to_modify.append([i,'i',received_missing_content[i]])
 
-    # Line insertion
-    for i in lines_to_insert:
-        file_content.insert(i-1,received_missing_content[i])
-    
-    # Line deletion
     for i in my_missing_content.keys():
-        file_content.pop(i-1)
+        contents_to_modify.append([i,'d'])
+    contents_to_modify.sort()
+
+    def update_lines(flag):
+        for i in range(len(contents_to_modify)):
+            if contents_to_modify[i][1]=='d':
+                contents_to_modify[i][0]+=flag
+        
+        
+    while contents_to_modify:
+        i=contents_to_modify.pop(0)
+        # Insert
+        if i[1]=='i':
+            file_content.insert(i[0]-1,i[2])
+            update_lines(1)
+        # Delete
+        else:
+            file_content.pop(i[0]-1)
+            update_lines(-1)
 
     # Write into file
     with open(filename,'w') as f:
