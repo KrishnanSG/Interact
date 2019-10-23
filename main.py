a6e9a64da12673fb047f98c6621fa851e565dfc6
KrishnanSG <krishsg525@gmail.com>
2019-10-23 15:02:08 +0530

    Add some cool loading text

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import argparse
 import os
 import sys
 import time
-from math import floor,log
+from math import floor, log
 
 from watchdog.events import PatternMatchingEventHandler
 from watchdog.observers import Observer
+from progress.spinner import Spinner
+from progress.bar import Bar
 
 from BloomFilter import BloomFilter
 from P2P.Server import NetworkManager
@@ -37,6 +39,7 @@ role = args.Role
 my_missing_content = {}
 should_trigger_modified = True
 
+
 class RequestReceivedHandler:
     def handle_request(self, request):
             global my_missing_content, should_trigger_modified
@@ -46,20 +49,25 @@ class RequestReceivedHandler:
                 # We send it now
                 print("\n\nThe other user has modified his file, syncing...")
                 print("Received the bloom filter")
-                my_missing_content = getMissingContent(getNFromSize(request.get_message_size()), request.get_message_bytes())
+                my_missing_content = getMissingContent(getNFromSize(
+                    request.get_message_size()), request.get_message_bytes())
                 print("Acknowleding and transmitting the bloom filter...")
                 bf = computeBloomFilter()
-                req = utils.Request(utils.Request.REQUEST_TYPE_REPLY_SLAVE_BLOOMFILTER, bf.getAsBytes())
+                req = utils.Request(
+                    utils.Request.REQUEST_TYPE_REPLY_SLAVE_BLOOMFILTER, bf.getAsBytes())
                 p2p.send_request(req)
 
             # p2p.send_data(bf.getAsBytes(), NetworkManager.REQUEST_ACKNOWLEDGE_SEND_BLOOMFILTER)
             elif(request.get_type() == utils.Request.REQUEST_TYPE_REPLY_SLAVE_BLOOMFILTER):
-                print("Request was acknowledged by the other peer and has given the other bloom filter")
-                my_missing_content = getMissingContent(getNFromSize(request.get_message_size()), request.get_message_bytes())
+                print(
+                    "Request was acknowledged by the other peer and has given the other bloom filter")
+                my_missing_content = getMissingContent(getNFromSize(
+                    request.get_message_size()), request.get_message_bytes())
 
-                ## Send the missing contents computed to the other user
+                # Send the missing contents computed to the other user
                 print("Sending the actual changed lines ...")
-                req = utils.Request(utils.Request.REQUEST_SEND_ACTUAL_LINES, str(my_missing_content))
+                req = utils.Request(
+                    utils.Request.REQUEST_SEND_ACTUAL_LINES, str(my_missing_content))
                 p2p.send_request(req)
 
             elif(request.get_type() == utils.Request.REQUEST_SEND_ACTUAL_LINES):
@@ -67,11 +75,13 @@ class RequestReceivedHandler:
                 missing_dict = eval(request.actual_message())
                 should_trigger_modified = False
                 print("Syncing the file...")
-                Synchronizer.syncFile(input_path, my_missing_content, missing_dict)
+                Synchronizer.syncFile(
+                    input_path, my_missing_content, missing_dict)
                 print("Verifying that the file is up-to-date ...")
                 print("Verifying hash...")
                 final_hash = Synchronizer.computeHash(input_path)
-                req = utils.Request(utils.Request.REQUEST_SEND_ENTIRE_FILE_HASH, final_hash)
+                req = utils.Request(
+                    utils.Request.REQUEST_SEND_ENTIRE_FILE_HASH, final_hash)
                 p2p.send_request(req)
                 print("Done.")
                 time.sleep(1)
@@ -89,7 +99,8 @@ class RequestReceivedHandler:
                 else:
                     print("Uh-oh")
                     print("Making sure things are fine everywhere...")
-                    req = utils.Request(utils.Request.REQUEST_SEND_ENTIRE_FILE, read_entire_file())
+                    req = utils.Request(
+                        utils.Request.REQUEST_SEND_ENTIRE_FILE, read_entire_file())
                     p2p.send_request(req)
             elif(request.get_type() == utils.Request.REQUEST_SEND_ENTIRE_FILE):
                 print("Looks like we have some trouble syncing ...")
@@ -103,27 +114,32 @@ class RequestReceivedHandler:
                 should_trigger_modified = True
                 print("Everything is back under control!")
 
+
 rh = RequestReceivedHandler()
 p2p = NetworkManager(rh)
 
 if not os.path.isfile(input_path):
-    print('\n',input_path, '- Not a valid file to stage for syncing')
+    print('\n', input_path, '- Not a valid file to stage for syncing')
     sys.exit()
+
 
 def on_modified(event):
     if (os.path.abspath(event.src_path) == input_path) and should_trigger_modified:
         # Detect changes from only the given path.
         # Ignore all other changes
-        print("\n\nDetected changes - ",event.src_path,"py has been modified...")
+        print("\n\nDetected changes - ",
+              event.src_path, "py has been modified...")
         initiateSync()
+
+
 class FileEventHandler(PatternMatchingEventHandler):
     def __init__(self, patterns=None, ignore_patterns=None, ignore_directories=False, case_sensitive=False,
                     on_modified_callback=on_modified):
-        self.on_modified_callback  = on_modified_callback
+        self.on_modified_callback = on_modified_callback
         self.last_modified = time.time()
         return super().__init__(patterns=patterns, ignore_patterns=ignore_patterns,
                             ignore_directories=ignore_directories, case_sensitive=case_sensitive)
-        
+
     def on_modified(self, event):
         if(time.time() - self.last_modified) > 1:
             self.on_modified_callback(event)
@@ -132,12 +148,16 @@ class FileEventHandler(PatternMatchingEventHandler):
 
 # Use this func to find n required for BloomFilter
 # Size is the len of bloomfilter bit array
+
+
 def getNFromSize(size):
     return(floor(size*-1*(log(2)**2)/log(0.05)))
 
+
 def main():
-    print("Starting server...")
-    print("Watching", input_path, "for changes...")
+    # spinner = Spinner('Intialising ',)
+    print("\nInteract [Version 1.0]")
+    print("GNU GENERAL PUBLIC LICENSE\nVersion 3, 23 Oct 2019\n")
     patterns = "*"
     ignore_patterns = ["*.save"]
     ignore_directories = True
@@ -152,15 +172,28 @@ def main():
     
     my_observer.start()
 
-    if role==1:
+    if role == 1:
+        progress = Bar("Creating Server")
+        for i in range(10):
+            # Do some work
+            time.sleep(0.05)
+            progress.next(10)
+        progress.finish()    
         p2p.create_host()
         initiateSync()
     else:
-        ip = input("Enter an IP: ")
-        #ip = '127.0.1.1' 
-        port = int(input("Enter a PORT: "))
+            
+        ip = input("Enter the IP of the host: ")
+        port = int(input("Enter the PORT of the host: "))
+        progress = Bar("Initiating Connection")
+        for i in range(10):
+            # Do some work
+            time.sleep(0.05)
+            progress.next(10)
+        progress.finish()
         p2p.create_client(ip,port)
 
+    print("Observing ", input_path, "for changes...")
 
     try:
         while True:
